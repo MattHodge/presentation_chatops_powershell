8022dd5341de2744f97326f9ac02b55b399254fa
Matthew Hodgkins <m.hodgkins@coolblue.nl>
2016-09-01 00:02:28 +0200

    initial

---
+++ demo_1_hubot_dsc/demo1.ps1
@@ -0,0 +1,80 @@
+$dscOutPath = 'C:\DSC'
+
+# Make a folder store the generated DSC configuration
+New-Item -Path $dscOutPath -Force -ItemType Directory
+
+$password = "vagrant" | ConvertTo-SecureString -AsPlainText -Force
+$cred =  New-Object -typename System.Management.Automation.PSCredential -ArgumentList 'vagrant', $password
+
+# Set the name of the machine that will host the bot
+$botserver = '172.28.128.100'
+
+$slackAPIKey = "TODO"
+
+#####################
+# Configure the LCM
+#####################
+
+# Configure LCM so we can manually run DSC and see what is happening
+[DSCLocalConfigurationManager()]
+configuration LCMConfig
+{
+    Node $botserver
+    {
+        Settings
+        {
+            ActionAfterReboot = 'StopConfiguration'
+        }
+    }
+}
+
+LCMConfig -OutputPath 'C:\DSC'
+
+Set-DscLocalConfigurationManager -Path $dscOutPath -ComputerName $botserver -Credential $cred
+
+#####################
+# DSC Local Apply
+#####################
+# Install Hubot locally
+Install-Module -Name Hubot
+
+# Let's look at the configuation example
+psedit 'C:\Program Files\WindowsPowerShell\Modules\Hubot\1.1.5\Examples\dsc_configuration.ps1'
+
+# Load the configuration example
+. 'C:\Program Files\WindowsPowerShell\Modules\Hubot\1.1.5\Examples\dsc_configuration.ps1'
+
+# Provide the configuration data
+$botConfigData = @{
+    AllNodes =
+    @(
+        @{
+            NodeName = $botserver;
+            Role = 'Hubot'
+            SlackAPIKey = $slackAPIKey
+            HubotAdapter = 'slack'
+            HubotBotName = 'demobot'
+            HubotBotPath = 'C:\myhubot'
+        }
+    )
+}
+
+# Generate the mof. $configData comes from dsc_configuration.ps1
+Hubot -OutputPath 'C:\DSC' -ConfigurationData $botConfigData
+
+Invoke-Command -ComputerName $botserver -Credential $cred -ScriptBlock {
+    Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+    Install-Module -Name Hubot -Force
+}
+
+Start-DscConfiguration -ComputerName $botserver -Credential $cred -Path $dscOutPath -Verbose -Wait
+
+Restart-Computer -ComputerName $botserver -Credential $cred -Wait -Force
+
+<# backup plan
+Invoke-Command -ComputerName $botserver -Credential $cred -ScriptBlock {
+    Restart-Computer -Force
+}
+#>
+
+Start-DscConfiguration -ComputerName $botserver -Credential $cred -UseExisting -Verbose -Wait
